e74ecb36a346bb7da10dedd1b4c140a36d7e4a19
kamgi <kamgidayatov@gmail.com>
2026-02-09 22:53:23 +0300

    KTB-02- Add try-catch handler

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -1,13 +1,26 @@
 package additional
 
 import java.io.File
+import java.io.FileNotFoundException
+import java.io.IOException
 
 fun main() {
 
     val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
 
-    for (word in wordsFile.readLines()) {
-        println(word)
+    try {
+        if (!wordsFile.exists()) {
+            wordsFile.createNewFile()
+        }
+    } catch (e: IOException) {
+        println("Ошибка при создании файла: ${e.message}")
+    }
+
+    try {
+        for (word in wordsFile.readLines()) {
+            println(word)
+        }
+    } catch (e: FileNotFoundException) {
+        println("Ошибка вывода строки: ${e.message}")
     }
 }
