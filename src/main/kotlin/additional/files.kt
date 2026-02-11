48f41bbe289cb34f20a7ede1c83a3d1e724d5faa
kamgi <kamgidayatov@gmail.com>
2026-02-11 17:58:25 +0300

    KTB-04- Parsing dictionary and print it

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -4,6 +4,12 @@ import java.io.File
 import java.io.FileNotFoundException
 import java.io.IOException
 
+data class Word(
+    val original: String,
+    val translate: String,
+    val correctAnswer: Int = 0,
+)
+
 fun main() {
 
     val wordsFile: File = File("words.txt")
@@ -16,10 +22,15 @@ fun main() {
         println("Ошибка при создании файла: ${e.message}")
     }
 
+    val dictionary: MutableList<Word> = mutableListOf()
+
     try {
-        for (word in wordsFile.readLines()) {
-            println(word)
+        for (line in wordsFile.readLines()) {
+            val line = line.split("|")
+            val word = Word(line[0], line[1], line[2].toIntOrNull() ?: 0)
+            dictionary.add(word)
         }
+        dictionary.forEach { println(it) }
     } catch (e: FileNotFoundException) {
         println("Ошибка вывода строки: ${e.message}")
     }
