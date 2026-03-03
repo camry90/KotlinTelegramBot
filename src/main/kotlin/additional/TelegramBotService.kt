a986453bad48fa7d6096827f54708e0877b7025b
kamgi <kamgidayatov@gmail.com>
2026-03-03 17:30:49 +0300

    KTB-15: realise fun sendMessage, get chat_id with RegEx and refactoring code

---
+++ src/main/kotlin/additional/TelegramBotService.kt
@@ -0,0 +1,27 @@
+package additional
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class TelegramBotService(val botToken: String) {
+
+    fun getUpdates(updateId: Int): String {
+        val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId"
+        val client: HttpClient = HttpClient.newBuilder().build()
+        val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+        val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        return response.body()
+    }
+
+    fun sendMessage(chatId: Int, text: String?) {
+        val urlSendUpdates = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$text"
+        val client: HttpClient = HttpClient.newBuilder().build()
+        val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendUpdates)).build()
+        val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
+
+        response.body()
+    }
+}
