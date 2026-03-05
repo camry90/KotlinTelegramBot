9e039d1a0b93f591be7a67811776dc1fb37f2b49
kamgi <kamgidayatov@gmail.com>
2026-03-05 14:33:11 +0300

    KTB-15: Fix smth

--- src/main/kotlin/additional/TelegramBotService.kt
+++ src/main/kotlin/additional/TelegramBotService.kt
@@ -10,7 +10,7 @@ const val BASIC_URL = "https://api.telegram.org/bot"
 
 class TelegramBotService(val botToken: String) {
 
-    val client: HttpClient = HttpClient.newBuilder().build()
+    private val client: HttpClient = HttpClient.newBuilder().build()
 
     fun getUpdates(updateId: Int): String {
         val urlGetUpdates = "$BASIC_URL$botToken/getUpdates?offset=$updateId"
@@ -22,7 +22,7 @@ class TelegramBotService(val botToken: String) {
 
     fun sendMessage(chatId: Long, text: String) {
         val encodedText = URLEncoder.encode(text, "UTF-8")
-        val urlSendUpdates = "$BASIC_URL/sendMessage?chat_id=$chatId&text=$encodedText"
+        val urlSendUpdates = "$BASIC_URL$botToken/sendMessage?chat_id=$chatId&text=$encodedText"
         val request: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlSendUpdates)).build()
         val response: HttpResponse<String> = client.send(request, HttpResponse.BodyHandlers.ofString())
 
